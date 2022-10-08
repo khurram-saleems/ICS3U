86ace117dd90870baa9a94721802a1c114bca2bb
Khurram Shaikh <shaikhkhurram962@gmail.com>
2022-10-08 18:39:09 -0400

    Update MCQuizQuestion.py

--- Unit 2/MCQuizQuestion.py
+++ Unit 2/MCQuizQuestion.py
@@ -1,20 +1,30 @@
-# 01 Practice Programs 1 
+# 06 Practice Programs 1 
 # Program 3: Multiple Choice Quiz Questions
 #
 # Khurram Shaikh
-# Monday, October 3, 2022
+# Friday, October 3, 2022
 #
 # This program asks user to respond to a multiple choice question,
 # displays a personalized sentence for each selection
 
-question = (input("What is 5^2? Enter 'A','B','C','D','E'\n(A) 50\n(B) 42\n(C) 125\n(D) 75\n(E) 25\n"))
-if (question=="A"):
+# Start infinite loop incase user enters invalid value
+while True:
+    
+    # Ask user what 5^2 is
+    answer = (input("What is 5^2? Enter 'A','B','C','D','E'\n(A) 50\n(B) 42\n(C) 125\n(D) 75\n(E) 25\n"))
+    
+    # If answer is not between 'A' and 'E' 
+    # Display error message
+    if (answer>="A" and answer<="E"):
+        break
+    print("Error! Please enter a value between 'A' and 'E'.")
+if (answer=="A"):
     print("50% of this is the correct answer, try again next time.")
-elif(question=="B"):
+elif(answer=="B"):
     print("42 is not the correct answer, it is a multiple of the factors 6 and 7.")
-elif(question=="C"):
+elif(answer=="C"):
     print("This is 5^3, not 5^2.")
-elif(question=="D"):
+elif(answer=="D"):
     print("That is three time's the actual answer.")
-elif(question=="E"):
+elif(answer=="E"):
     print("Nice, 25 is the correct answer!")
