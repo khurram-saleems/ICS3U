3326eff7092d23835e905a58d991293c03871d96
Khurram Shaikh <shaikhkhurram962@gmail.com>
2022-10-08 18:21:03 -0400

    Update Tax Calc.py

--- Unit 2/Tax Calc.py
+++ Unit 2/Tax Calc.py
@@ -2,16 +2,36 @@
 # Program 1: Tax Calculator
 #
 # Khurram Shaikh
-# Friday, September 30, 2022
+# Friday, October 7, 2022
 #
 # This program asks user to enter the cost of 
 # food and drinks, then calculates the total.
 # If total is greater than 15, tax is displayed
 # and included in the total
 
-print("{:-^80}".format(""))
-foodCost = float(input("Enter the cost of food: "))
-drinkCost = float(input("Enter the drink cost: "))
+# Start infinite loop in case user enters invalid value
+while True:
+    
+    # Ask user for cost of food
+    foodCost = float(input("Enter the cost of food: "))
+    
+    # If cost is less than 5.99 display error message
+    if (foodCost>=5.99):
+        break
+    print("Error! Food items are all $5.99 or greater.")
+
+# Start infinite loop incase user enters invalid value
+while True:
+    
+    # Ask user for drink cost
+    drinkCost = float(input("Enter the drink cost: "))
+    
+    # If cost is less than 1.99 display error message
+    if (drinkCost>=1.99):
+        break
+    print("Error! Drink items are $1.99 or greater.")
+
+# Calculate total cost
 total=foodCost+drinkCost
 
 # If total is greater than 15 calculate tax,
