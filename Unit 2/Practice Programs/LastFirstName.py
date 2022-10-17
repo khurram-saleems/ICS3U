df6205ceb64f246d26fc0e743f0f8585c93cd377
khurramshaikh <khurramshaikhwork@gmail.com>
2022-10-16 23:54:57 -0400

    Update LastFirstName.py

--- Unit 2/Practice Programs/LastFirstName.py
+++ Unit 2/Practice Programs/LastFirstName.py
@@ -7,9 +7,16 @@
 # This program asks the user to enter 10 names, outputs 
 # the name that is alphebetically last.
 
-low=""
+lastName=""
+
+# Start counted loop to repeat 10 times
 for x in range(10):
+    
+    # Ask user for first name
     name=input("Enter your first name: ")
-    if (name>low):    
-        low=name
-print("The name that is alphebetically last is {}".format(low))
+    
+    # If name greater than low 
+    # Make name equal low
+    if (name>lastName):    
+        lastName=name
+print("The name that is alphebetically last is {}".format(lastName))
