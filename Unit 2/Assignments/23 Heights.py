a1beb299b23647fde622307476dd87f8681c5328
khurramshaikh <khurramshaikhwork@gmail.com>
2022-10-18 17:43:03 -0400

    finish stuff

---
+++ Unit 2/Assignments/23 Heights.py
@@ -0,0 +1,68 @@
+#     Assignment | Assignment 2-3
+#  Program Title | Class Average Heights
+#                |
+#     Written by | Khurram Shaikh
+#           Date | Wednesday, October 19, 2022
+#                |
+#    Description | This program asks for the heights of students in 
+#                | a class, then displays the class's average height.
+
+classes=1
+
+# Start infinite loop
+while True: 
+    sum=0
+    print("{:*^80s}".format("Class Average Height Calculator"))
+    
+    # Start infinite loop in case user enters invalid value
+    while True:
+        
+        # Ask for amount of students
+        students=int(input("Enter amount of students ('0' to exit): "))
+        
+        # If amount is not a positive number display error message
+        if (students>=0):
+            break
+        print("Error invalid student amount, enter a positive value!")
+    
+    # If amount is equal to 0 exit program
+    if (students==0):
+        break
+    
+    # Display prompt for height
+    print("\nEnter height:")
+    
+    # Start counted loop to total amount of students
+    for x in range(students):
+        
+        # Start infinite loop in case user enters invalid value
+        while True:
+            
+            # Ask user for height
+            height=int(input(""))
+
+            # If height is less than 150 and greater than 220 
+            # display error message
+            if (height>=150 and height<=220):
+                break
+            print("Error invalid height, enter a value between 150 and 220!")
+        
+        # Add height and store in sum
+        sum+=height
+    avg=sum/students
+    
+    # Display average height to one decimal place
+    print("Average height is: {:.1f} cm".format(avg),end=" ")
+    
+    # If classes are greater than 1 display total classes 
+    if (classes>1):
+        print("and a total of {} classes".format(classes))    
+    while True:
+        restart=input("\nWould you like calculate the average height again? Enter 'y' or 'n': ")
+        if (restart=='n' or restart=='y'):
+            break
+        print("Error invalid response, enter 'y' or 'n'.")
+    if (restart=='n'):
+        break
+    else:
+        classes+=1
--- Unit 2/Practice Programs/MakingMoney.py
+++ Unit 2/Practice Programs/MakingMoney.py
@@ -1,11 +1,13 @@
 # 10 Practice Programs 2
-# Program 8: Last First Name 
+# Program 8: Making Money
 #
 # Khurram Shaikh
 # Friday, October 14, 2022
 #
 # Prog desc
-balance,rate=1000,0.06
+
+balance,newBalance,interest=1000,0,0
 for x in range(1,11):
-    balance
-    print("Year {}, ")
+    interest+=balance*0.06
+    newBalance+=interest+balance
+    print("Year {}, initial balance ${:.2f}, interest ${:.2f} and new total ${:.2f}.".format(x,newBalance,interest,newBalance))
