c904e8f6aa16e145bc03fbf7cc0cfbeb2605891d
khurramshaikh <khurramshaikhwork@gmail.com>
2022-10-27 08:03:08 -0400

    Update MakingMoney.py

--- Unit 2/Practice Programs/MakingMoney.py
+++ Unit 2/Practice Programs/MakingMoney.py
@@ -1,13 +1,7 @@
-# 10 Practice Programs 2
-# Program 8: Making Money
-#
-# Khurram Shaikh
-# Friday, October 14, 2022
-#
-# Prog desc
-
-balance,newBalance,interest=1000,0,0
-for x in range(1,11):
-    interest+=balance*0.06
-    newBalance+=interest+balance
-    print("Year {}, initial balance ${:.2f}, interest ${:.2f} and new total ${:.2f}.".format(x,newBalance,interest,newBalance))
+y,d,rte=0,1000,0.06
+for x in range(10):
+    y+=1
+    interest=rte*d
+    ib=d
+    d+=interest
+    print(y,"${:.2f} ${:.2f} ${:.2f}".format(interest,ib,d))
