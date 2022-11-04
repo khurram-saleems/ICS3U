a7bdeb0be43a12482063cc9416af725dbd1ca296
khurramshaikh <khurramshaikhwork@gmail.com>
2022-11-03 21:12:18 -0400

    made some changes

---
+++ Unit 3/Assignments/31 Shrinking.py
@@ -0,0 +1,7 @@
+#     Assignment | Assignment 3-1
+#  Program Title | Shrinking Sentence 
+#                |
+#     Written by | Khurram Shaikh
+#           Date | Wednesday, November 2, 2022
+#                |
+#    Description | 
--- Unit 3/Assignments/ok.py
+++
@@ -1,3 +0,0 @@
-sentence=input("Enter a sentence: ")
-for i in range(0,len(sentence),-1):
-    print(sentence[i])
