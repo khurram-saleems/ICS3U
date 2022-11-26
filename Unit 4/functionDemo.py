06ee05dc814d5615dd693b322d5808ff14a95778
Khurram Shaikh <shaikhkhurram962@gmail.com>
2022-11-26 16:25:09 -0500

    fixed up some gh issues

---
+++ Unit 4/functionDemo.py
@@ -0,0 +1,7 @@
+def pressEnter():
+    print("______________________________________")
+    input("Please press Enter to continue: ")
+    print()
+pressEnter()
+pressEnter()
+pressEnter()
---
+++ Unit 4/validValPr.py
@@ -0,0 +1,9 @@
+def validVal(x,y):
+    while True:
+        v=int(input("Enter a value: "))
+        if (v>=x and v<=y):
+            break
+        print('error')
+    return v
+choice=validVal(1,5)
+print(choice)
