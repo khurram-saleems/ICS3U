fe7abbf51f377f929a358c4e034d44a088ee8cbf
khurramshaikh <khurramshaikhwork@gmail.com>
2022-11-27 14:19:42 -0500

    Update ShowTitleDemo.py

---
+++ Unit 4/Practice Programs/ShowTitleDemo.py
@@ -0,0 +1,8 @@
+# 01 Practice Programs 1 
+# Program 1: Clear 
+#
+# Khurram Shaikh
+# Friday, October 14, 2022
+#
+# This program classifies athletes into three classes
+# entirely based on weight.
