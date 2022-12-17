1b11c8b6bc98ab9f4c1c7fcb159c726a076a6d95
Khurram Shaikh <shaikhkhurram962@gmail.com>
2022-12-17 00:18:49 -0500

    Create GroceryList.py

---
+++ Unit 5/Assignments/GroceryList.py
@@ -0,0 +1,7 @@
+#     Assignment | Assignment 5-2
+#  Program Title | Grocery List
+#                |
+#     Written by | Khurram Shaikh
+#           Date | Friday, December 9, 2022
+#                |
+#    Description | This program
